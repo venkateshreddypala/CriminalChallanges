6f5bd9fc5f277141dd6be8e55dfc22ff98e43849
venkateshreddypala <venkateshreddypala@gmail.com>
2017-10-29 15:00:25 -0400

    ch1sol

--- app/src/main/java/com/example/venkateshpala/criminalchallanges/QuizActivity.java
+++ app/src/main/java/com/example/venkateshpala/criminalchallanges/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.venkateshpala.criminalchallanges;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,7 +20,10 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                Toast toast=
+                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP,0,0);
+                toast.show();
 
             }
         });
@@ -27,7 +31,10 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                Toast toast=
+                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                 toast.setGravity(Gravity.TOP, 0,0);
+                toast.show();
 
             }
         });
